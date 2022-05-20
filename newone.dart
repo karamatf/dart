9402c881326a762bceb475ece0aa6db8ac285030
karamatf <karamatf@gmail.com>
2022-05-20 23:22:31 +0200

    Add files via upload

---
+++ newone.dart
@@ -0,0 +1,169 @@
+void main() {
+  var listOfWinningApps2012 = [
+    "FNB",
+    "Discovery HealthID",
+    "Matchy",
+    "Phraza",
+    "Plascon",
+    "RapidTargets",
+  ];
+  var listOfWinningApps2013 = [
+    "Bookly",
+    "Deloitte Digital",
+    "DStv",
+    "Gautrain buddy",
+    "Kids Aid",
+    "Markitshare",
+    "Nedbank",
+    "PriceCheck",
+    "SnapScan",
+  ];
+
+  var listOfWinningApps2014 = [
+    "LIVE Inspect",
+    "My Belongings",
+    "Rea Vaya",
+    "Zapper",
+    "Sync Mobile",
+    "Vigo",
+    "SuperSport",
+    "Wildlife Tracker",
+  ];
+
+  var listOfWinningApps2015 = [
+    "CPUT Mobile",
+    "DStv Now",
+    "EskomSepush",
+    "M4JAM",
+    "Vula Mobile",
+    "WumDrop",
+  ];
+
+  var listOfWinningApps2016 = [
+    "Domestly",
+    "hearZA",
+    "iKhokha",
+    "MiBrand",
+    "KaChing",
+    "Tuta-Me",
+    "Friendly Math MOnsters for kindergarten",
+  ];
+
+  var listOfWinningApps2017 = [
+    "OrderIn",
+    "Pick 'n Pays Super Animals",
+    "Hey Jude",
+    "InterGreatMe",
+    "EcoSlips",
+    "Zulzi",
+    "ORU Social TouchSA",
+    "Awethu Project",
+    "Shyft",
+    "WatIf Health Portal",
+    "TransUnion 1Check",
+  ];
+
+  var listOfWinningApps2018 = [
+    "Cowa Bunga",
+    "Bestee",
+    "AGCL",
+    "StokFella",
+    "Xander English",
+    "Ctrl",
+    "Pineapple",
+    "besmarta",
+    "ASI Snakes",
+  ];
+
+  var listOfWinningApps2019 = [
+    "Digger",
+    "SI Realities",
+    "Vula Mobile",
+    "Hydra Mobile",
+    "Matric Live",
+    "Franc",
+    "LocTransi",
+    "Naked Insurance",
+    "LootDefence",
+    "MoWash",
+    "Over",
+    "My Pregnancy Journey",
+  ];
+
+  var listOfWinningApps2020 = [
+    "Examsta",
+    "Checkers Sixty60",
+    "Technisen",
+    "BirdPro",
+    "Lexie Hearing",
+    "League of Legends",
+    "GreenFingers Mobile",
+    "Xitsonga Dictionary",
+    "StokFella",
+    "Bottles",
+    "Matric Live",
+    "Gaurdian Health",
+    "EasyEquities",
+    "My Pregnancy Journey",
+  ];
+
+  var listOfWinningApps2021 = [
+    "iiDENTIFii",
+    "HelloPay SoftPos",
+    "Gaurdian Health Platform",
+    "Ambani Africa",
+    "Roadsave",
+    "Rekindle",
+    "Shyft",
+    "UniWise",
+    "Kazi",
+    "Murimi",
+    "Takealot",
+    "SiSa",
+  ];
+
+  var overallWinner2017 = "Shyft";
+  var shyft2017 =
+      "Shyft is the money app that gives you more for less, 24/7. Buy forex instantly anytime, anywhere, and at the best rates, and invest in the top U.S. stocks and ETFs directly from your phone.";
+  var overallWinner2018 = "Khula";
+  var khula2018 =
+      "Tools and platforms to support and grow your business in the agriculture supply chain";
+  var totalnumbersoffapps = "Total numbers of apps 94";
+
+  listOfWinningApps2012.sort();
+  print('2012 $listOfWinningApps2012');
+
+  listOfWinningApps2013.sort();
+  print('2013 $listOfWinningApps2013');
+
+  listOfWinningApps2014.sort();
+  print('2014 $listOfWinningApps2014');
+
+  listOfWinningApps2015.sort();
+  print('2015 $listOfWinningApps2015');
+
+  listOfWinningApps2016.sort();
+  print('2016 $listOfWinningApps2016');
+
+  listOfWinningApps2017.sort();
+  print('2017 $listOfWinningApps2017');
+
+  listOfWinningApps2018.sort();
+  print('2018 $listOfWinningApps2018');
+
+  listOfWinningApps2019.sort();
+  print('2019 $listOfWinningApps2019');
+
+  listOfWinningApps2020.sort();
+  print('2020 $listOfWinningApps2020');
+
+  listOfWinningApps2021.sort();
+  print('2021 $listOfWinningApps2021');
+
+  print('Overal Winner 2017 $overallWinner2017');
+  print(shyft2017);
+
+  print('Overal Winner 2018 $overallWinner2018');
+  print(khula2018);
+  print(totalnumbersoffapps);
+}
